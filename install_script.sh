de365cac0ef3f8b5fa5bd7f82a7f841161484271
xad001x0w <james@jegproductions.co.uk>
2024-06-13 15:47:07 +0100

    Fix small typo

--- install_script.sh
+++ install_script.sh
@@ -77,10 +77,13 @@ Installexmp()
 
 Setautostart()
 {
-	if [ ! -f /home/pi/.config/lxsession/LXDE-pi/autostart ] then
+	if [ ! -f /home/pi/.config/lxsession/LXDE-pi/autostart ]; then
+		
     	echo "Copying default autostart file..."
     	cp /etc/xdg/lxsession/LXDE-pi/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
+	
 	fi
+
 	echo "Inserting autostart line..."
 	grep -qxF "/usr/bin/python3 /home/pi/pipresents" /home/pi/.config/lxsession/LXDE-pi/autostart || echo "/usr/bin/python3 /home/pi/pipresents" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 	echo "...done!"
